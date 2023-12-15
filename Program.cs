66e44762b2a624205d3c8f738e7dfd172478f786
Artem Voitenko <bobinrobin12@gmail.com>
2023-12-15 13:53:31 +0300

    Program complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод массива с клавиатуры
+        Console.WriteLine("Введите элементы массива, разделяя их запятой:");
+        string input = Console.ReadLine();
+        string[] array = input.Split(',');
+
+        // Формирование нового массива
+        string[] newArray = new string[array.Length];
+        int newIndex = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newArray[newIndex] = array[i];
+                newIndex++;
+            }
+        }
+
+        // Вывод нового массива
+        Console.Write("[");
+        for (int i = 0; i < newIndex; i++)
+        {
+            Console.Write(newArray[i]);
+            if (i < newIndex - 1)
+            {
+                Console.Write(",");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
